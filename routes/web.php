d3bfe2ebf38efadd344d61a8de80297c9babf935
leninepestana <leninepestanana@gmail.com>
2020-07-02 22:06:12 +0100

    Updating Posts Layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('blog.index');
 });
+Route::get('/blog/show', function () {
+    return view('blog.show');
+});
